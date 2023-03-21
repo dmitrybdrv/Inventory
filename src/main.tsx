b6c6c3ac4845183640e38c26a5ef61a3d9a04c10
dmitrybdrv <incubator-it@protonmail.com>
2023-03-21 15:36:26 +0300

    inventory2

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {Provider} from "react-redux";
 import {store} from "./store/Store";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
     <Provider store={store}>
         <BrowserRouter>
             <Inventory />
